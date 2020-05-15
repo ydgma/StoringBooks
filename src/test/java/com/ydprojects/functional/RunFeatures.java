a65772f2090c2860e9cb3deab702455c25ebb8dd
ydgma <45771853+ydgma@users.noreply.github.com>
2020-05-15 10:40:44 +0100

    Develop (#11)
    * Springintegration (#8)
    
    * fixed issue with form not displaying items
    
    * quick commit before merging with develop
    
    * got basic spring functionality implemented for saving a book
    
    * Springintegration (#10)
    
    * fixed issue with form not displaying items
    
    * quick commit before merging with develop
    
    * got basic spring functionality implemented for saving a book
    
    * removed absolute property file path
    
    * made the feature tests failing pass with the cucumber spring dependency
    
    * added a new view to add UTF8 books
    
    * quick commit before merging with develop

--- src/test/java/com/ydprojects/functional/RunFeatures.java
+++ src/test/java/com/ydprojects/functional/RunFeatures.java
@@ -1,12 +1,15 @@
 package com.ydprojects.functional;
 
 
+import com.ydprojects.config.AppConfig;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features", glue="")
+@ContextConfiguration(classes= AppConfig.class)
 public class RunFeatures {
 
 }
