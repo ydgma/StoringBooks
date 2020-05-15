a374e824352529556692d9fef1eb9a5f4665b7e5
Yasiru Dahanayake <yasirudahanayake@gmail.com>
2020-05-15 10:28:12 +0100

    quick commit before merging with develop

--- src/main/java/com/ydprojects/controller/BookController.java
+++ src/main/java/com/ydprojects/controller/BookController.java
@@ -2,11 +2,8 @@ package com.ydprojects.controller;
 
 import com.ydprojects.dao.BookDAOImpl;
 import com.ydprojects.entity.book.BookImpl;
-import com.ydprojects.entity.book.BookType;
 import com.ydprojects.entity.book.PDF;
 import com.ydprojects.entity.book.UTF8;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -56,5 +52,4 @@ public class BookController {
         bookDAO.addBook(utf8);
         return "redirect:/";
     }
-
 }
